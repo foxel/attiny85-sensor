c773d38e33ede4219a993ba91ae24602cce08a05
Andrey F. Kupreychik <foxel@quickfox.ru>
2018-07-28 16:03:08 +0700

    Updated to comply newest library - stability fix

--- attiny85-sensor.ino
+++ attiny85-sensor.ino
@@ -11,7 +11,7 @@
 #define DHTPIN      3
 #define PIRPIN      2
 #define BINDPIN     1
-#define SENSOR_ADDR 0xAA55
+#define SENSOR_ADDR 0xAA56
 
 // each period is about 8 seconds
 #define SLEEP_PERIODS 3
@@ -20,11 +20,13 @@ dht dht22;
 NooliteTX tx(RFPIN, SENSOR_ADDR);
 
 int8_t cntr = 0;
+int8_t oldPIR = 0;
 
 void setup()
 {
   DDRB = 0x00; // Set direction to input on all pins
   PORTB = 0xFF; // Enable pull-ups on pins 
+  ADCSRA &= ~_BV(ADEN); // disable ADC
 
   // bind
   pinMode(BINDPIN, INPUT);
@@ -34,7 +36,9 @@ void setup()
     tx.send_command(15);
     digitalWrite(PWRPIN, LOW);
   }
-  pinMode(PIRPIN, INPUT);
+  pinMode(PIRPIN, INPUT_PULLUP);
+  digitalWrite(PWRPIN, LOW);
+  pinMode(PWRPIN, OUTPUT);
   
   power_all_disable();
 
@@ -43,62 +47,65 @@ void setup()
   WDTCR |= _BV(WDIE);
   GIMSK |= _BV(PCIE);
   PCMSK |= _BV(PIRPIN);
-  sei();
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  send_pir();
+  sei();
 }
 
 ISR (WDT_vect) {
   WDTCR |= _BV(WDIE);
-  if (!cntr--) {
-    send_readings();
-    cntr = SLEEP_PERIODS;
+  if (cntr) {
+    cntr--;
   }
 }
 
 // PIR sensor
-ISR(PCINT0_vect)
-{
-  send_pir();
-}
+ISR(PCINT0_vect) {}
 
 void loop()
 {
+  wdt_reset();
+  lock_interrupts();
+  // PIR
+  int newPIR = digitalRead(PIRPIN);
+  if (newPIR ^ oldPIR) {
+    digitalWrite(PWRPIN, HIGH);
+    tx.send_command(newPIR == HIGH ? 2 : 0);
+    oldPIR = newPIR;
+  }
+  // sensor
+  if (!cntr) {
+    digitalWrite(PWRPIN, HIGH);
+    if (dht22.read(DHTPIN) == DHTLIB_OK) {
+      int deci_temp = int(dht22.temperature * 10);
+      byte deci_hum = byte(dht22.humidity);
+  
+      uint8_t tx_args[] = {
+        deci_temp & 0xff,
+        (deci_temp >> 8) & 0x0f | 0x20,
+        deci_hum,
+        0xff // can be some analog sensor
+      };
+    
+      tx.send_command(21, tx_args, 4);
+    } else {
+      tx.send_command(255);
+    }
+    cntr = SLEEP_PERIODS;
+  }
+  
+  digitalWrite(PWRPIN, LOW);
+  unlock_interrupts();
+  power_all_disable();
   sleep_enable();
   sleep_bod_disable();
   sleep_cpu();
 }
 
-void send_readings()
-{
-  cli();
-  digitalWrite(PWRPIN, HIGH);
-  if (dht22.read(DHTPIN) == DHTLIB_OK) {
-    int deci_temp = int(dht22.temperature * 10);
-    byte deci_hum = byte(dht22.humidity);
-
-    uint8_t tx_args[] = {
-      deci_temp & 0xff,
-      (deci_temp >> 8) & 0x0f | 0x20,
-      deci_hum,
-      0xff // can be some analog sensor
-    };
-  
-    tx.send_command(21, tx_args, 4);
-  } else {
-    tx.send_command(255);
-  }
-  digitalWrite(PWRPIN, LOW);
-  sei();
+void lock_interrupts() {
+  GIMSK &= ~_BV(PCIE);
 }
 
-void send_pir()
-{
-  cli();
-  int PIRState = digitalRead(PIRPIN);
-
-  digitalWrite(PWRPIN, HIGH);
-  tx.send_command(PIRState == HIGH ? 2 : 0);
-  digitalWrite(PWRPIN, LOW);
-  sei();
+void unlock_interrupts() {
+  GIMSK |= _BV(PCIE);
 }
+
