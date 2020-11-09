8b1549cad598f73396f52201e05e38934bbc2266
Andrey F. Kupreychik <foxel@quickfox.ru>
2020-11-09 19:31:11 +0700

    2020 updates

--- attiny85-sensor.ino
+++ attiny85-sensor.ino
@@ -12,15 +12,16 @@
 #define PIRPIN      2
 #define BINDPIN     1
 #define SENSOR_ADDR 0xAA56
+#define TX_REPEATS  3
 
 // each period is about 8 seconds
-#define SLEEP_PERIODS 3
+#define SLEEP_PERIODS 6
 
 dht dht22;
-NooliteTX tx(RFPIN, SENSOR_ADDR);
+NooliteTX tx(RFPIN, SENSOR_ADDR, TX_REPEATS);
 
 int8_t cntr = 0;
-int8_t oldPIR = 0;
+int8_t oldPIR = HIGH;
 
 void setup()
 {
@@ -37,9 +38,14 @@ void setup()
     digitalWrite(PWRPIN, LOW);
   }
   pinMode(PIRPIN, INPUT_PULLUP);
+  if (digitalRead(BINDPIN) == LOW) {
+      // assume PIR sensor is in place and we don't need pull-up protection
+      pinMode(PIRPIN, INPUT);
+  }
+
   digitalWrite(PWRPIN, LOW);
   pinMode(PWRPIN, OUTPUT);
-  
+
   power_all_disable();
 
   wdt_reset();
@@ -59,7 +65,12 @@ ISR (WDT_vect) {
 }
 
 // PIR sensor
-ISR(PCINT0_vect) {}
+ISR(PCINT0_vect) {
+  // force sensor data transmission if the last sleep period is in progress
+  if (cntr == 1) {
+    cntr--;
+  }
+}
 
 void loop()
 {
@@ -72,27 +83,35 @@ void loop()
     tx.send_command(newPIR == HIGH ? 2 : 0);
     oldPIR = newPIR;
   }
+
   // sensor
   if (!cntr) {
-    digitalWrite(PWRPIN, HIGH);
-    if (dht22.read(DHTPIN) == DHTLIB_OK) {
+    uint8_t battery_value = read_battery();
+
+    if (battery_value < 150) {  // 3 volts
+      digitalWrite(PWRPIN, HIGH);
+      tx.send_command(20);
+    } else if (dht22.read(DHTPIN) == DHTLIB_OK) {
       int deci_temp = int(dht22.temperature * 10);
       byte deci_hum = byte(dht22.humidity);
-  
+
       uint8_t tx_args[] = {
         deci_temp & 0xff,
         (deci_temp >> 8) & 0x0f | 0x20,
         deci_hum,
-        0xff // can be some analog sensor
+        battery_value
       };
-    
+
+      wait(100); // Wait for power to settle
+      digitalWrite(PWRPIN, HIGH);
       tx.send_command(21, tx_args, 4);
     } else {
+      digitalWrite(PWRPIN, HIGH);
       tx.send_command(255);
     }
     cntr = SLEEP_PERIODS;
   }
-  
+
   digitalWrite(PWRPIN, LOW);
   unlock_interrupts();
   power_all_disable();
@@ -109,3 +128,27 @@ void unlock_interrupts() {
   GIMSK |= _BV(PCIE);
 }
 
+uint8_t read_battery() {
+  power_adc_enable();
+  ADCSRA = _BV(ADEN); // eanable ADC
+  ADMUX = _BV(MUX3) | _BV(MUX2); // Vcc as ref, 1.1 as input
+  wait(100); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // start ADC
+  while(ADCSRA & (1<<ADSC)); // Wait for conversion to complete.
+  uint8_t low = ADCL; // read value
+  uint8_t high = ADCH; // ...
+  int raw_value = (high << 8) | low;
+  ADCSRA = 0x00; // disable ADC
+  power_adc_disable();
+
+  return 50 * 1.1 * 1024.0/raw_value;
+}
+
+// rough one
+void wait(unsigned int ms) {
+  if (ms < 65) {
+    delayMicroseconds(ms * 1000);
+  } else {
+    while (ms--) delayMicroseconds(1000);
+  }
+}
